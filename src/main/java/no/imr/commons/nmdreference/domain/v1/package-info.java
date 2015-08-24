bbe2998dafaa4651fa8edcfe3d7f409a7d539ad9
KjetilFjellheim <kjetil.fjellheim@imr.no>
2015-08-24 13:21:07 +0200

    Initial commit after renaming

---
+++ src/main/java/no/imr/commons/nmdreference/domain/v1/package-info.java
@@ -0,0 +1,4 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.imr.no/formats/nmdreference/v1", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED, xmlns = {
+    @javax.xml.bind.annotation.XmlNs(namespaceURI = "http://www.imr.no/formats/nmdreference/v1", prefix = "")
+})
+package no.imr.commons.nmdreference.domain.v1;
